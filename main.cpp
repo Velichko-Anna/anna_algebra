5885272a6f6dcc0ce01d2c63ef6ff923a79c6c15
Анна Величко <annavelicko@iMac-Anna.local>
2020-12-25 21:48:13 +0300

    Задача

--- main.cpp
+++ main.cpp
@@ -1,1 +1,27 @@
 # anna_algebra
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    
+    cout << "Введите число, на которое умножим строку ";
+    int k, n, i, b;
+    cin >> k;
+    vector <int> a;
+    cout << "Сколько в строке элементов? ";
+    cin >> n;
+    for (i = 0; i < n; i++)
+    {
+        cout << "Введите элемент строки ";
+        cin >> b;
+        a.push_back(b * k);
+    }
+    cout << "Умноженная строка: ";
+    for (i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
