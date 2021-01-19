273e460883ffd33da774bf0a7ac4bfcbc992ef40
Анна Величко <annavelicko@iMac-Anna.local>
2021-01-19 12:07:15 +0300

    Функция вывода

--- zadacha.2.cpp
+++ zadacha.2.cpp
@@ -1,1 +1,34 @@
-#anna_algebra
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector <int> VYVOD(vector <int> X)
+{
+    for (auto now : X)
+    {
+        cout << now << " ";
+    }
+    return X;
+}
+
+int main()
+{
+    //ВВОД СТРОКИ
+    cout << "Сколько элементов в строке? ";
+    int n, i;
+    vector <int> A;
+    cin >> n;
+    for (i = 0; i < n; i++)
+    {
+        int a;
+        cout << "Введите элемент строки: ";
+        cin >> a;
+        A.push_back(a);
+    }
+    
+    VYVOD(A);
+    cout << " \n";
+    
+    return 0;
+}
+
