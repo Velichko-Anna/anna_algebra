da4b6a7dc58dae8aa9b9de2fad1e4dbaed3147f1
Анна Величко <annavelicko@iMac-Anna.local>
2021-02-07 22:44:12 +0300

    Сама задача, куда вызывать функцию из файла

---
+++ zadacha.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include "print.h"
+using namespace std;
+
+
+
+int main()
+{
+    
+    vector <int> a = {1, 2, 3, 4, 5};
+    vector <int> s = PRINT(a);
+   
+    return 0;
+    
+}
+
