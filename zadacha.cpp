0e8d12f77259512a274e5cee0ca3aae6f2bee0f4
Анна Величко <annavelicko@iMac-Anna.local>
2021-02-09 20:51:59 +0300

    Файл .cpp

---
+++ zadacha.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include "print.h"
+using namespace std;
+
+
+int main()
+{
+    vector <int> a = {1, 2, 3, 4, 5};
+    PRINT(a);
+    cout << "\n";
+   
+    return 0;
+}
+
