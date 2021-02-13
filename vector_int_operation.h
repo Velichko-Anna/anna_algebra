0b4ba8da3031eeb56013eb9a16fcd9cc93d0ed44
Анна Величко <annavelicko@iMac-Anna.local>
2021-02-13 12:11:47 +0300

    Функции умножения, сложения и вычитания строк .h

---
+++ vector_int_operation.h
@@ -0,0 +1,10 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <vector>
+
+std::vector <int> Multiply(std::vector <int> a, int k);
+std::vector <int> Summa(std::vector <int> a, std::vector <int> b);
+std::vector <int> Subtract(std::vector <int> a, std::vector <int> b);
+
+#endif  
