6247adf8e180493e3b30bea5a56a8bcf0676dc46
Анна Величко <annavelicko@iMac-Anna.local>
2021-02-13 12:09:03 +0300

    Вызов функций для вектора

---
+++ zadacha_vector.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <iostream>
+#include "test_vector_int_operation.h"
+using namespace std;
+
+
+int main()
+{
+    TestMultiply();
+    TestSumma();
+    TestSubtract();
+   
+    return 0;
+}
+
