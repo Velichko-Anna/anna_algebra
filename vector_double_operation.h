b121a35e725979dc69d7ee76fdcd80a1e16f1e63
Анна Величко <annavelicko@iMac-Anna.local>
2021-02-17 22:24:55 +0300

    Функции умножения, сложения и вычитания для даблов

---
+++ vector_double_operation.h
@@ -0,0 +1,10 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <vector>
+
+std::vector <double> Multiply(std::vector <double> a, double k);
+std::vector <double> Summa(std::vector <double> a, std::vector <double> b);
+std::vector <double> Subtract(std::vector <double> a, std::vector <double> b);
+
+#endif
