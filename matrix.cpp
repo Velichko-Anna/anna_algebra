2255db9e1c765aa7c365d85b22c090d5a79d3e7c
Анна Величко <annavelicko@iMac-Anna.local>
2021-03-14 15:40:27 +0300

    Операции над матрицами

--- matrix.cpp
+++ matrix.cpp
@@ -1,51 +1,57 @@
-#include "vector_double_operation.h
+#include "vector_double_operation.h"
+#include "matrix.h"
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-vector < vector <double> > MatrixMultiply(vector <vector <double> matrix, double k)
+//УМНОЖЕНИЕ СТРОКИ МАТРИЦЫ
+vector < vector <double> > MatrixMultiply(vector < vector <double> > matrix, int index, double k)
 {
-  vector < vector <double> > result(matrix.size);
-  for (int i = 1; i < matrix.size; i++)
+  vector < vector <double> > result(matrix.size());
+  for (int i = 1; i < matrix.size(); i++)
   {
-    result[i] = Multiply(matrix[i], k);
-  }
-  return result;
-}
-
-vector < vector <double> > MatrixSumma(vector <vector <double> matrix1, vector <vector <double> matrix2)
-{
-  vector < vector <double> > result(matrix1.size);
-  for (int i = 1; i < matrix1.size; i++)
-  {
-    result[i] = Summa(matrix1[i], matrix2[i]);
-  }
-  return result;
-}
-
-vector < vector <double> > MatrixSubstract(vector <vector <double> matrix1, vector <vector <double> matrix2)
-{
-  vector < vector <double> > result(matrix1.size);
-  for (int i = 1; i < matrix1.size; i++)
-  {
-    result[i] = Substract(matrix1[i], matrix2[i]);
-  }
-  return result;
-}
-
-vector < vector <double> > MatrixMultipy1(vector <vector <double> matrix, int index, double k)
-{
-  vector < vector <double> > result(matrix.size);
-  for (int i = 1; i < matrix.size; i++)
-  {
-    if (i = index)
+    if (i == index)
     {
       result[i] = Multiply(matrix[i], k);
     }
     else
     {
       result[i] = matrix[i];
+    }
+  }
+  return result;
+}
+
+vector < vector <double> > MatrixSumma(vector < vector <double> > matrix, int index1, int index2)
+{
+  vector < vector <double> > result(matrix.size());
+  for (int i = 1; i < matrix.size(); i++)
+  {
+    if (i == index1)
+    {
+        result[i] = Summa(matrix[index1], matrix[index2]);
+    }
+    else
+    {
+        result[i] = matrix[i];
+    }
+  }
+  return result;
+}
+
+vector < vector <double> > MatrixSubtract(vector < vector <double> > matrix, int index1, int index2)
+{
+  vector < vector <double> > result(matrix.size());
+  for (int i = 1; i < matrix.size(); i++)
+  {
+    if (i == index1)
+    {
+        result[i] = Subtract(matrix[i], matrix[index2]);
+    }
+    else
+    {
+        result[i] = matrix[i];
     }
   }
   return result;
