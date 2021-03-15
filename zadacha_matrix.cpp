233e6309c3b743924880fd7644ab659789c7bcb9
Анна Величко <annavelicko@iMac-Anna.local>
2021-03-15 15:37:58 +0300

    Вызов функций для матрицы

---
+++ zadacha_matrix.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <iostream>
+#include "test_matrix.h"
+using namespace std;
+
+
+int main()
+{
+    TestMultiplyMatrix();
+    TestSummaMatrix();
+    TestSubtractMatrix();
+    
+    return 0;
+}
+
