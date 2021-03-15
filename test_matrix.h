2c3ec81bab8873f4ee626e04bbe506a4a64e5795
Анна Величко <annavelicko@iMac-Anna.local>
2021-03-15 15:38:59 +0300

    Тест для операций на матрицами .h

---
+++ test_matrix.h
@@ -0,0 +1,13 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <vector>
+using namespace std; 
+
+void TestMultiplyMatrix();
+void TestSummaMatrix();
+void TestSubtractMatrix();
+void TestChangeMatrix();
+
+
+#endif
