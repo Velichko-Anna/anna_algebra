336a45c9b5fc2e2f9c28906056f4e87a4e50c3f6
Анна Величко <annavelicko@iMac-Anna.local>
2021-03-21 10:12:09 +0300

    Операции над матрицами .h

--- matrix.h
+++ matrix.h
@@ -4,10 +4,17 @@
 #include <vector>
 using namespace std;
 
-vector < vector <double> > MatrixMultiply(vector < vector <double> > matrix, int ind, double k);
-vector < vector <double> > MatrixSumma(vector < vector <double> > matrix, int index1, int index2);
-vector < vector <double> > MatrixSubtract(vector < vector <double> > matrix, int index1, int index2);
-vector < vector <double> > MatrixChange(vector < vector <double> > matrix, int index1, int index2);
+//УМНОЖЕНИЕ СТРОКИ МАТРИЦЫ
+vector <vector <double>> MatrixMultiply(vector <vector <double>> matrix, int ind, double k);
+
+//СЛОЖЕНИЕ СТРОК МАТРИЦЫ
+vector <vector <double>> MatrixSumma(vector <vector <double>> matrix, int index, vector <double> line);
+
+//РАЗНОСТЬ СТРОК МАТРИЦЫ
+vector <vector <double>> MatrixSubtract(vector <vector <double>> matrix, int index, vector <double> line);
+
+//ПЕРЕСТАНОВКА СТРОК
+vector <vector <double>> MatrixChange(vector <vector <double>> matrix, int index1, int index2);
 
 
 #endif
