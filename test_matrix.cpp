7cdbb496933eb79ec4509a804fd8a03999933b4d
Анна Величко <annavelicko@iMac-Anna.local>
2021-03-21 10:12:53 +0300

    Тесты для операций над матрицами

--- test_matrix.cpp
+++ test_matrix.cpp
@@ -47,48 +47,40 @@ void TestSummaMatrix()
     vector <double> line4 = {1.1, 2.2, 3.3};
     int index4 = 1;
     vector <vector <double>> result4 = MatrixSumma(matrix4, index4, line4);
-    assert((result4 == vector <vector<double>> {{1.1, 2.2, 3.3}, {2.2, 4.4, 6.6}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}})).
+    assert((result4 == vector <vector <double>> {{1.1, 2.2, 3.3}, {2.2, 4.4, 6.6}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}}));
     
-    //СЛОЖЕНИЕ ВТОРОЙ И ТРЕТЬЕЙ СТРОКИ (ТРЕТЬЯ НУЛЕВАЯ)
-    vector < vector <double> > matrix5 = {{1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}};
+    //СЛОЖЕНИЕ ТРЕТЬЕЙ СТРОКИ С НУЛЕВОЙ СТРОКОЙ
+    vector <vector <double>> matrix5 = {{1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}};
     vector <double> line5 = {0, 0, 0};
-    int index5 = 1;
-    vector < vector <double> > result5 = MatrixSumma(matrix5, index5, line5);
-    assert((result5 == vector < vector <double> > { {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3} } ));
+    int index5 = 2;
+    vector <vector <double>> result5 = MatrixSumma(matrix5, index5, line5);
+    assert((result5 == vector <vector <double>> {{1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}}));
 }
     
-    /*СЛОЖЕНИЕ ВТОРОЙ И ТРЕТЬЕЙ СТРОКИ (ВТОРАЯ НУЛЕВАЯ)
-    vector < vector <double> > matrix6 = { {1.1, 2.2, 3.3}, {0, 0, 0}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3} };
-    vector < vector <double> > result6 = MatrixSumma(matrix6, 2, 3);
-    assert((result6 == vector < vector <double> > { {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3} } ));
-    
-    //СЛОЖЕНИЕ ПЕРВОЙ И ЧЕТВЕРТОЙ СТРОКИ
-    vector < vector <double> > matrix7 = { {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3} };
-    vector < vector <double> > result7 = MatrixSumma(matrix7, 1, 4);
-    assert((result7 == vector < vector <double> > { {2.2, 4.4, 6.6}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3} } ));
-}
 
 //ТЕСТЫ РАЗНОСТИ
 void TestSubtractMatrix()
 {
     //РАЗНОСТЬ ВТОРОЙ И ТРЕТЬЕЙ СТРОКИ
-    vector < vector <double> > matrix8 = { {1.1, 2.2, 3.3}, {2.2, 4.4, 6.6}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3} };
-    vector < vector <double> > result8 = MatrixSubtract(matrix8, 2, 3);
-    assert((result8 == vector < vector <double> > { {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3} } ));
+    vector <vector <double>> matrix8 = {{1.1, 2.2, 3.3}, {2.2, 4.4, 6.6}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}};
+    vector <double> line8 = {1.1, 2.2, 3.3};
+    int index8 = 1;
+    vector <vector <double>> result8 = MatrixSubtract(matrix8, index8, line8);
+    assert((result8 == vector <vector <double>> {{1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}}));
 }
 
-//ТЕСТ ЕРЕСТАНОВКА СТРОК
+//ТЕСТ ПЕРЕСТАНОВКА СТРОК
 void TestChangeMatrix()
 {
     //ПЕРЕСТАНОВКА ВТОРОЙ И ТРЕТЬЕЙ СТРОКИ
-    vector < vector <double> > matrix9 = { {1.1, 2.2, 3.3}, {2.2, 4.4, 6,6}, {1, 2, 3}, {1.1, 2.2, 3.3} };
-    vector < vector <double> > result9 = MatrixChange(matrix9, 2, 3);
-    assert((result9 == vector < vector <double> > { {1.1, 2.2, 3.3}, {1, 2, 3}, {2.2, 4.4, 6.6}, {1.1, 2.2, 3.3} } ));
+    vector <vector <double>> matrix9 = {{1.1, 2.2, 3.3}, {2.2, 4.4, 6,6}, {1, 2, 3}, {1.1, 2.2, 3.3}};
+    vector <vector <double>> result9 = MatrixChange(matrix9, 2, 3);
+    assert((result9 == vector <vector <double>> {{1.1, 2.2, 3.3}, {1, 2, 3}, {2.2, 4.4, 6.6}, {1.1, 2.2, 3.3}}));
     
     //ПЕРЕСТАНОВКА ПЕРВОЙ И ЧЕТВЕРТОЙ СТРОКИ
-    vector < vector <double> > matrix10 = { {2.2, 4.4, 6.6}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}, {1, 2, 3} };
-    vector < vector <double> > result10 = MatrixChange(matrix10, 1, 4);
-    assert((result10 == vector < vector <double> > { {1, 2, 3}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}, {2.2, 4.4, 6.6} } ));
-}*/
+    vector <vector <double>> matrix10 = {{2.2, 4.4, 6.6}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}, {1, 2, 3}};
+    vector <vector <double>> result10 = MatrixChange(matrix10, 1, 4);
+    assert((result10 == vector <vector <double>> {{1, 2, 3}, {1.1, 2.2, 3.3}, {1.1, 2.2, 3.3}, {2.2, 4.4, 6.6}}));
+}
 
 
